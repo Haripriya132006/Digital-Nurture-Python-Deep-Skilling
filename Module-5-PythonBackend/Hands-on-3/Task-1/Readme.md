91dbdce2164c39a94dfdf2ca4e6c51f79e108971
Haripriya132006 <haripriyaks13@gmail.com>
2026-07-03 22:13:58 +0530

    new tasks

--- Module-5-PythonBackend/Hands-on-3/Task-1/Readme.md
+++ Module-5-PythonBackend/Hands-on-3/Task-1/Readme.md
@@ -1,3 +1,4 @@
+```
 from rest_framework import serializers
 from .models import Department,Course,Student,Enrollment
 
@@ -19,3 +20,12 @@ class EnrollmentSerializer(serializers.ModelSerializer):
           class Meta:
                     model=Enrollment
                     fields='__all__'
+```
+
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
+![alt text](image-4.png)
+![alt text](image-5.png)
+![alt text](image-6.png)
--- Module-5-PythonBackend/coursemanager/courses/views.py
+++ Module-5-PythonBackend/coursemanager/courses/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render ,HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+from .models import Course
+from .serializers import CourseSerializer
 
 # Create your views here.
 def hello_view(request):
           return HttpResponse("Course Management API is running")
+
+class CourseListView(APIView):
+          def get(self,request):
+                    courses=Course.objects.all()
+                    serializer=CourseSerializer(courses,many=True)
+                    return Response(serializer.data)
+
+          def post(self,request):
+                    serializer=CourseSerializer(data=request.data)
+                    if serializer.is_valid():
+                              serializer.save()
+                              return Response(serializer.data,status=status.HTTP_201_CREATED)
+                    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class CourseDetailView(APIView):
+          def get(self,request,pk):
+                    course=get_object_or_404(Course,pk=pk)
+                    serializer=CourseSerializer(course)
+                    return Response(serializer.data)
+          
+          def put(self,request,pk):
+                    course=get_object_or_404(Course,pk=pk)
+                    serializer=CourseSerializer(course,data=request.data)
+                    if serializer.is_valid():
+                              serializer.save()
+                              return Response(serializer.data)
+                    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+          
+          def delete(self,request,pk):
+                    course=get_object_or_404(Course,pk=pk)
+                    course.delete()
+                    return Response(status=status.HTTP_204_NO_CONTENT)
+          
